b7e4a5268d8c94e51a39bfbb95a41fe4ad2744d8
Chik3r <ikervaraalarcon@gmail.com>
2021-05-28 14:28:52 -0400

    Remove hitbox getter On hook

--- TinyPlayerHItbox.cs
+++ TinyPlayerHItbox.cs
@@ -11,50 +11,11 @@ namespace TinyPlayerHItbox
 {
 	public class TinyPlayerHItbox : Mod
 	{
-		// private static MethodInfo _hitboxGetMethodCache = null;
-		//
-		// private static MethodInfo HitboxGetInfo
-		// {
-		// 	get
-		// 	{
-		// 		if (_hitboxGetMethodCache == null)
-		// 			_hitboxGetMethodCache = typeof(Entity).GetProperty(nameof(Entity.Hitbox)).GetGetMethod();
-		//
-		// 		return _hitboxGetMethodCache;
-		// 	}
-		// }
-		//
-		// private delegate Rectangle OrigHitboxGetter(Entity self);
-		//
-		// private delegate Rectangle HookHitboxGetter(OrigHitboxGetter orig, Entity self);
-		
-		// private static event HookHitboxGetter OnHitboxGetter
-		// {
-		// 	add => HookEndpointManager.Add(HitboxGetInfo, value);
-		// 	remove => HookEndpointManager.Remove(HitboxGetInfo, value);
-		// }
-
 		public override void Load()
 		{
-			// IL.Terraria.Player.getRect += PlayerOngetRect;
 			IL.Terraria.Player.Update_NPCCollision += PlayerOnUpdate_NPCCollision;
 			On.Terraria.Player.getRect += PlayerOngetRect;
-			// OnHitboxGetter += CustomHitboxGetter;
 		}
-
-		public override void Unload()
-		{
-			// OnHitboxGetter -= CustomHitboxGetter;
-			// _hitboxGetMethodCache = null;
-		}
-		
-		// private Rectangle CustomHitboxGetter(OrigHitboxGetter orig, Entity p)
-		// {
-		// 	if (!(p is Player player))
-		// 		return orig(p);
-		// 	
-		// 	return CalculateNewRect(player);
-		// }
 		
 		private Rectangle PlayerOngetRect(On.Terraria.Player.orig_getRect orig, Player self)
 		{
@@ -79,18 +40,6 @@ namespace TinyPlayerHItbox
 			Error:
 				Logger.Error("Failed to IL edit Update_NPCCollision");
 		}
-		
-		// private void PlayerOngetRect(ILContext il)
-		// {
-		// 	ILCursor c = new ILCursor(il);
-		// 	c.Emit(OpCodes.Ldarg_0);
-		// 	c.EmitDelegate<Func<Player, Rectangle>>(p =>
-		// 	{
-		// 		return CalculateNewRect(p);
-		// 		return new Rectangle((int) p.position.X, (int) p.position.Y, p.width, p.height);
-		// 	});
-		// 	c.Emit(OpCodes.Ret);
-		// }
 
 		private Rectangle CalculateNewRect(Player p)
 		{
